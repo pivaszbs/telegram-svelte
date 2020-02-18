aca08e046e955b6e14c0c15a3fe6bcf2e9848bf5
Anton Krylov <batya@Antons-MacBook-Pro.local>
2020-02-18 18:03:35 +0300

    dialog

---
+++ src/components/dialog/dialog.svelte
@@ -0,0 +1,20 @@
+<script>
+    import RightTop from './right-top.svelte';
+    import RightBottom from './right-bottom.svelte';
+    export let avatar;
+    export let count;
+    export let name;
+    export let short;
+    export let time;
+    export let pinned;
+    export let out;
+    export let read;
+</script>
+
+<div class="dialog">
+    <div class="avatar"></div>
+    <div class="name">{name}</div>
+    <div class="short">{short}</div>
+    <RightTop {out} {read}/>
+    <RightBottom {time} {pinned} />
+</div>
---
+++ src/components/dialog/right-top.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let time;
+    export let out;
+    export let read;
+</script>
+
+<div class="right-top">
+    {#if out}
+        <div class="out">
+            {#if read}
+                 <!-- svg -->
+            {:else}
+                 <!-- another svg -->
+            {/if}
+        </div>
+    {/if}
+    <div class="time">{time}</div>
+</div>
