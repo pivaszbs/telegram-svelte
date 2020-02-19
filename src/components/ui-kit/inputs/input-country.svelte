62e7ec54f2a5e8a66b3ebf9e3095615127d79421
Anton Krylov <batya@Antons-MacBook-Pro.local>
2020-02-19 18:37:06 +0300

    inputs

---
+++ src/components/ui-kit/inputs/input-country.svelte
@@ -0,0 +1,11 @@
+<script>
+    import {country} from '../../../stores/inputs';
+</script>
+
+<style>
+    /* your styles go here */
+</style>
+<div class="input-group">
+    <input bind:value={$country} type="country">
+    
+</div>
---
+++ src/pages/login-form.svelte
@@ -0,0 +1,19 @@
+<script>
+    import CountryInput from '../components/ui-kit/inputs/input-country.svelte';
+    import PhoneInput from '../components/ui-kit/inputs/input-phone.svelte';
+    import CountryPopup from '../components/ui-kit/popups/popup-countries.svelte';
+    import Checkbox from '../components/checkbox.svelte';
+    import Button from '../components/button.svelte';
+</script>
+
+<div class="login-page">
+	<div class="login-form">
+		<img src="../../static/images/logo.png" alt="Telegram logo" class="login-form__icon">
+		<h1 class="login-form__title">Sign in to Telegram</h1>
+		<h3 class="login-form__description">Please confirm your country and enter your phone number</h3>
+		<CountryInput />
+        <PhoneInput />
+        <Checkbox />
+        <Button>NEXT</Button>
+	</div>
+</div>
